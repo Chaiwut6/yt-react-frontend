dbf42017a630e3a910cb20dbff7eca35e30cf96d
Chaiwut <chivwut0646@gmail.com>
2024-02-04 17:40:12 +0700

    edit position

--- src/components/Tags/Tags.jsx
+++ src/components/Tags/Tags.jsx
@@ -3,7 +3,7 @@ import './Tags.css'
 function Tags() {
   return (
     <section id ="tags">
-      <a href=""className='-item -active'>
+      <a href=""className='-item '>
       ทั้งหมด
       </a>
       <a href=""className='-item'>
